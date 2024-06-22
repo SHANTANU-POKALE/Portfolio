952ee6e2702e6e0d3eaac22914f4cf0cdec011c7
shantanu pradip pokale <shantanupokale009@gmail.com>
2024-06-22 11:58:48 +0530

    pass

--- send_email.py
+++ send_email.py
@@ -8,7 +8,7 @@ def send_email(message):
     port = 465
 
     username = "shantanupokale009@gmail.com"
-    password = st.secrets["PASSWORD"]
+    password = "wsth ajna scam cunt"
 
     receiver = "shantanupokale009@gmail.com"
     context = ssl.create_default_context()
